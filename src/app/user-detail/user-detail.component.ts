b01aef45b7f2a8ae0e1eec4213e5479345b3c1b8
Daniel Johannsen <daniel-johannsen@web.de>
2022-11-10 22:08:58 +0100

    included deleteUser button

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { User } from 'src/models/user.class';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogEditAddressComponent } from '../dialog-edit-address/dialog-edit-address.component';
 import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-user-detail',
@@ -18,7 +19,9 @@ export class UserDetailComponent implements OnInit {
 
   constructor(private firestore: AngularFirestore, 
     private route: ActivatedRoute, 
-    public dialogRef: MatDialog) { }
+    public dialogRef: MatDialog,
+    private router: Router,
+    private _location: Location) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap =>  {
@@ -54,6 +57,23 @@ export class UserDetailComponent implements OnInit {
     const dialog = this.dialogRef.open(DialogEditUserComponent);
     dialog.componentInstance.user = new User(this.user.toJSON());
     dialog.componentInstance.userId = this.userId;
+  }
+
+
+  back(): void {
+    this._location.back();
+  }
+
+
+  deleteUser() {
+    if (this.userId) {
+      this.firestore
+        .collection('users')
+        .doc(this.userId)
+        .delete();
+        
+      this.back();
+    }
   }
 
 }
